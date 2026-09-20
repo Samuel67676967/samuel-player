aadb2ba04703ddf5c6ab8bc164d1f442828ee22e
Samuel67676967 <saiffbiking@gmail.com>
2026-09-20 16:28:34 +0400

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,6 @@ YDL_SEARCH_OPTS = {
     "extract_flat": "in_playlist",
     "skip_download": True,
     "default_search": "ytsearch",
-    "extractor_args": {"youtube": {"player_client": ["android"]}},
     **_cookie_opts,
 }
 
@@ -25,7 +24,6 @@ YDL_STREAM_OPTS = {
     "format": "bestaudio/best",
     "skip_download": True,
     "noplaylist": True,
-    "extractor_args": {"youtube": {"player_client": ["android"]}},
     **_cookie_opts,
 }
 
